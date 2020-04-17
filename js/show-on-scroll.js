254c8e26da6f461491394250b28a0bd88eb7603f
Kieran <kieran@Kieran-Sh-MacBook-Pro.local>
2020-04-17 18:08:04 +0100

    Added JS to all pages

--- js/show-on-scroll.js
+++ js/show-on-scroll.js
@@ -4,10 +4,16 @@ var elementsToShow = document.querySelectorAll('.show-on-scroll');
 
     Array.prototype.forEach.call(elementsToShow, function(element, index){
       if (isElementInViewport(element)) {
-        setTimeout(function(){element.classList.add('is-visible');},index*200)
-      } else {
-        element.classList.remove('is-visible');
+        setTimeout(
+		function(){
+			if (isElementInViewport(element)) {
+			element.classList.add('is-visible');
+			}
+    },index*100)
       }
+      // else {
+      //   element.classList.remove('is-visible');
+      // }
     });
 }
 
@@ -34,46 +40,3 @@ function isElementInViewport(el) {
 $(window).scroll(function(){
     checkAnimation();
 });
-
-
-
-// // Detect request animation frame
-// var scroll = window.requestAnimationFrame ||
-//              // IE Fallback
-//              function(callback){ window.setTimeout(callback, 1000/60)};
-// var elementsToShow = document.querySelectorAll('.show-on-scroll');
-//
-// function loop() {
-//
-//     Array.prototype.forEach.call(elementsToShow, function(element){
-//       if (isElementInViewport(element)) {
-//         element.classList.add('is-visible');
-//       } else {
-//         element.classList.remove('is-visible');
-//       }
-//     });
-//
-//     scroll(loop);
-// }
-//
-// // Call the loop for the first time
-// loop();
-//
-// // Helper function from: http://stackoverflow.com/a/7557433/274826
-// function isElementInViewport(el) {
-//   // special bonus for those using jQuery
-//   if (typeof jQuery === "function" && el instanceof jQuery) {
-//     el = el[0];
-//   }
-//   var rect = el.getBoundingClientRect();
-//   return (
-//     (rect.top <= 0
-//       && rect.bottom >= 0)
-//     ||
-//     (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
-//       rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-//     ||
-//     (rect.top >= 0 &&
-//       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-//   );
-// }
